0562f2cb626aef1283b39af61607944ce5100bb7
Vladislav Klymenko <vladislaviwe@outlook.com>
2022-12-04 18:41:27 +0200

    task completed

---
+++ README.md
@@ -0,0 +1,11 @@
+LIST:
+https://ibb.co/RjFKQQT
+
+GET:
+https://ibb.co/sqYcv2D
+
+ADD:
+https://ibb.co/MS40Wm7
+
+REMOVE:
+https://ibb.co/mG6NTbH
---
+++ index.js
@@ -0,0 +1,42 @@
+const { program } = require("commander");
+
+const contacts = require("./contacts");
+
+const invokeAction = async({action, id, name, email, phone}) => {
+    switch (action) {
+        case "list":
+            const allContacts = await contacts.listContacts();
+            console.table(allContacts);
+            break;
+
+        case "get":
+            const oneContact = await contacts.getContactById(id);
+            console.log(oneContact);
+            break;
+
+        case "add":
+            const newContact = await contacts.addContact({name, email, phone});
+            console.log(newContact);
+            break;
+
+        case "remove":
+            const deleteContact = await contacts.removeContact(id);
+            console.log(deleteContact);
+            break;
+    
+        default:
+            console.warn("\x1B[31m Unknown action type!");
+    }
+}
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+const options = program.opts();
+
+invokeAction(options);
